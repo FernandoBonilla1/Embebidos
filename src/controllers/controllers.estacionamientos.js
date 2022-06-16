710194101fa97fccb9e8bc602566777765a9cd68
Fernando Bonilla <fbr009@alumnos.ucn.cl>
2022-06-15 20:53:27 -0400

    prueba 1

--- src/controllers/controllers.estacionamientos.js
+++ src/controllers/controllers.estacionamientos.js
@@ -1,19 +1,35 @@
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const connection = require("../config/db")
 
 const getEstacionamientos = async (req, res) => {
     try{
-        const cuadrantes = await connection.query('SELECT * FROM estacionamiento');
-        if (cuadrantes.rows.length === 0) {
+        const estacionamiento = await connection.query('SELECT * FROM estacionamiento');
+        if (estacionamiento.rows.length === 0) {
             res.status(200).json({
                 msg: "No hay estacionamiento"
             })
         }
-        res.status(200).json({ cuadrantes: cuadrantes.rows });
+        res.status(200).json({ estacionamientos: estacionamiento.rows });
     } catch(error){
         res.status(500).json({
             msg: "No se pudo acceder a la tabla estacionamiento",
             error
         })
+    }
+}
+
+const updateEstacionamiento = async (req, res) =>{
+    try{
+        const {distancia} = req.body
+        if(distancia < 100){
+
+        }else{
+
+        }
+
+    } catch (error){
+
     }
 }
 
--- src/controllers/controllers.secciones.js
+++ src/controllers/controllers.secciones.js
@@ -2,13 +2,13 @@ const connection = require("../config/db")
 
 const getSecciones = async (req, res) => {
     try{
-        const cuadrantes = await connection.query('SELECT * FROM seccion');
-        if (cuadrantes.rows.length === 0) {
+        const secciones = await connection.query('SELECT * FROM seccion');
+        if (secciones.rows.length === 0) {
             res.status(200).json({
                 msg: "No hay seccion"
             })
         }
-        res.status(200).json({ cuadrantes: cuadrantes.rows });
+        res.status(200).json({ secciones: secciones.rows });
     } catch(error){
         res.status(500).json({
             msg: "No se pudo acceder a la tabla seccion",
