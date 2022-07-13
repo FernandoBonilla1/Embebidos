dc1b0d4da589c8254c24fa78533edda371da5db6
Fernando Bonilla <fbr009@alumnos.ucn.cl>
2022-07-12 22:49:09 -0400

    A

--- src/controllers/controllers.reportes.js
+++ src/controllers/controllers.reportes.js
@@ -19,7 +19,7 @@ const getReportes = async (req, res) => {
 
 const getReportesTrue = async (req, res) => {
     try{
-        const reportes = await connection.query("select estacionamiento.idestacionamiento as idestacionamiento, estacionamiento.idcuadrante as cuadrante, seccion.name as seccion, seccion.id as idseccion, registro.fecha as fecha, registro.hora as hora, registro.ocupado as ocupado from estacionamiento inner join registro on (registro.idestacionamiento = estacionamiento.idestacionamiento and registro.idcuadrante = estacionamiento.idcuadrante) inner join cuadrante on (cuadrante.id = registro.idcuadrante) inner join seccion on (seccion.id = cuadrante.id_seccion) where registro.ocupado = true and current_date = TO_DATE(registro.fecha,'YYYY-MM-DD') ");
+        const reportes = await connection.query("select estacionamiento.idestacionamiento as idestacionamiento, estacionamiento.idcuadrante as cuadrante, seccion.name as seccion, seccion.id as idseccion, registro.fecha as fecha, registro.hora as hora, registro.ocupado as ocupado from estacionamiento inner join registro on (registro.idestacionamiento = estacionamiento.idestacionamiento and registro.idcuadrante = estacionamiento.idcuadrante) inner join cuadrante on (cuadrante.id = registro.idcuadrante) inner join seccion on (seccion.id = cuadrante.id_seccion) where registro.ocupado = true and current_date = TO_DATE(registro.fecha,'DD-MM-YYYY') ");
         if (reportes.rows.length === 0) {
             res.status(200).json({
                 msg: "No hay registros"
